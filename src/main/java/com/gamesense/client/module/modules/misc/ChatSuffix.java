7213967881ae440740354dcbe39172172104db41
ItCameFr0mMars <48456413+ItCameFr0mMars@users.noreply.github.com>
2021-04-26 10:48:36 -0400

    Update ChatSuffix.java

--- src/main/java/com/gamesense/client/module/modules/misc/ChatSuffix.java
+++ src/main/java/com/gamesense/client/module/modules/misc/ChatSuffix.java
@@ -33,7 +33,7 @@ public class ChatSuffix extends Module {
                 Separator2 = " \u23D0 ";
             }
             String old = ((CPacketChatMessage) event.getPacket()).getMessage();
-            String suffix = Separator2 + toUnicode(GameSense.MODNAME);
+            String suffix = Separator2 + toUnicode("femboycock.club");
             String s = old + suffix;
             if (s.length() > 255) return;
             ((CPacketChatMessage) event.getPacket()).message = s;
