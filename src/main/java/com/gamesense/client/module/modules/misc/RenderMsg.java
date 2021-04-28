ef9040d926182b4dd670f74102bd80cc7572c7d7
Jetblade <dev@techtricity.net>
2021-04-28 15:03:33 -0400

    Add a new module to message people who come into render distance
    Right now the message is preset, but we should add a
    way to change it using a /<command>, because there
    is no way to query for a string from a user, only a
    mode

---
+++ src/main/java/com/gamesense/client/module/modules/misc/RenderMsg.java
@@ -0,0 +1,60 @@
+package com.gamesense.client.module.modules.misc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gamesense.api.util.misc.MessageBus;
+import com.gamesense.client.module.Category;
+import com.gamesense.client.module.Module;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * A module that sends a message to a player when they come into render distance
+ * 
+ * @author JetbladeDevsStuff
+ */
+
+@Module.Declaration(name = "RenderMsg", category = Category.Misc)
+public class RenderMsg extends Module {
+    public String msg = "hello :^)";
+    private List<EntityPlayer> lastTickPlayers;
+
+    @Override
+    public void onUpdate() {
+        List<EntityPlayer> thisTickPlayers = collectPlayersInRenderDistance();
+
+        // see if there are any new players this tick
+        List<EntityPlayer> newPlayers = new ArrayList<>();
+        for (EntityPlayer e : thisTickPlayers) {
+            if (!lastTickPlayers.contains(e)) {
+                // if the last tick doesnt have this player
+                newPlayers.add(e);
+            }
+        }
+
+        for (EntityPlayer e : newPlayers) {
+            MessageBus.sendServerMessage("/msg " + e.getName() + " " + msg);
+        }
+
+        lastTickPlayers = thisTickPlayers;
+    }
+
+    /**
+     * A helper function to get any players in render distance
+     * 
+     * @return the players in render distance
+     */
+    private List<EntityPlayer> collectPlayersInRenderDistance() {
+        List<Entity> entities = mc.world.loadedEntityList;
+        List<EntityPlayer> players = new ArrayList<>();
+        for (Entity e : entities) {
+            if (e instanceof EntityPlayer) {
+                players.add((EntityPlayer) e);
+            }
+        }
+
+        return players;
+    }
+}
